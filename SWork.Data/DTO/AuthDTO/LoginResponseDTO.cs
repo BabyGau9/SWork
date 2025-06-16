1b456c5fa9c5ca1543c6cf5b3cecb38a3c70f028
Duc Trieu <buiductrieu0810@gmail.com>
2025-06-16 13:34:41 +0700

    fix: add firstName-lastName

--- SWork.Data/DTO/AuthDTO/LoginResponseDTO.cs
+++ SWork.Data/DTO/AuthDTO/LoginResponseDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using SWork.Data.DTO.AuthDTO;
@@ -9,6 +10,8 @@ namespace SWork.Data.DTO.AuthDTO
     public class LoginResponseDTO
     {
         public UserDTO? User { get; set; }
+
+
         public IEnumerable<string> Role { get; set; } = new List<string>();
         public string Token { get; set; } = string.Empty;
         public string RefreshToken { get; set; } = string.Empty;
